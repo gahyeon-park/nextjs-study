aca78d5da14e53cde490626162b1a71acbd39f84
gahyeon <pgh2031@inpix.com>
2023-09-21 13:28:16 +0900

    4.17 제품링크 완성

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,16 @@
 import Link from "next/link";
 
+const products = ["shirt", "pants", "skirt", "shoes"];
+
 export default function Products() {
-  return <div>
-    <h2>Products</h2>
-    <ul>
-      <li>
-        <Link href="/products/skirt">skirt</Link>
-      </li>
-      <li>
-        <Link href="/products/pants">pants</Link>
-      </li>
-      <li>
-        <Link href="/products/shoes">shoes</Link>
-      </li>
-      <li>
-        <Link href="/products/outer">outer</Link>
-      </li>
-    </ul>
-  </div>
+  return (
+    <>
+      <h2>Products</h2>
+      <ul>
+        {products.map((product, index) =>
+          <li key={index}><Link href={`/products/${product}`}>{product}</Link></li>
+        )}
+      </ul>
+    </>
+  )
 }
